b61e28712017db337be2d9b5bdd66b12d628eff9
kalmisto <konsta.susi@gmail.com>
2011-10-03 21:25:25 +0300

    Yleinen tiedostonimi, tiedoston koon katsominen (mahdollisesti rikki)

--- englanti-suomi.c
+++ englanti-suomi.c
@@ -1,7 +1,31 @@
+#include <sys/stat.h>
+
 #include <err.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <fcntl.h>
+
+#define SANA_TIEDOSTO "/home/kalle/programs/sanastoja/sanoja.txt"
+
+struct sana {
+	char suomi[128];
+	char eng[128];
+};
+
+
+/* Return size of the file 'fname */
+size_t
+get_filesize()
+{
+	struct stat sb;
+
+	if (stat(SANA_TIEDOSTO, &sb) < 0) {
+		fprintf(stderr, "Tiedoston avaus epäonnistui\n");
+		exit(2);
+	}
+
+	return sb.st_size;
+}
 
 
 int
@@ -10,15 +34,18 @@ main(int argc, const char *argv[])
 	FILE	*sanatiedosto;
 	char	rivi[BUFSIZ];
 
-	sanatiedosto = fopen("/home/kalle/programs/sanastoja/sanoja.txt", "r");
+	sanatiedosto = fopen(SANA_TIEDOSTO, "r");
 	if (sanatiedosto == NULL) {
 		fprintf(stderr, "Tiedoston avaus epäonnistui\n");
 		exit(1);
 	}
 
+	fprintf(stdout, "Tiedoston koko tavuina: %lld\n", (long long) get_filesize());
+	/*
 	while ((fgets(rivi, BUFSIZ - 1, sanatiedosto)) != NULL) {
 		fprintf(stdout, "%s", rivi);
 	}
+	*/
 
 	if (sanatiedosto != NULL)
 		fclose(sanatiedosto);
